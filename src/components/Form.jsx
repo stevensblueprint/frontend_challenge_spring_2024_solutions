d7c522d0b869ffb8519f46892661b5c85f6c2521
Christian Apostol <christiand.apostol@outlook.com>
2024-09-21 15:15:26 -0400

    basic form structure

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -1,21 +1,48 @@
 const Form = () => {
+
+  const handleSubmission = () => {
+    console.log("SUBMITTED")
+  }
+
   return (
     <>
-      <form action={() => console.log("SUBMITTED")}>
-          <label for="name">Name</label>
-          <input type="text" name="name"/>
+      <div className="flex flex-col">
+        <h1>Stevens Blueprint Frontend Assessment S2024</h1>
 
-          <label for="email">Email</label>
-          <input type="text" name="email" />
+        <h2>Christian & Shawn's Custom Form</h2>
 
-          <label for="graduationYear">Graduation Year</label>
-          <input type="radio" name="graduationYear" value="2025"/> 2025
-          <input type="radio" name="graduationYear" value="2026"/> 2026
-          <input type="radio" name="graduationYear" value="2027"/> 2027
-          <input type="radio" name="graduationYear" value="2028"/> 2028
+        <h3>
+          Please submit your information in the form, 
+          so that it can be displayed below!
+        </h3>
 
-          <button type="submit">Submit</button>
-      </form>
+        <form action={handleSubmission}>
+            <label for="name">Name</label>
+            <input type="text" name="name"/>
+
+            <label for="email">Email</label>
+            <input type="text" name="email" />
+
+            <label for="graduationYear">Graduation Year</label>
+            <input type="radio" name="graduationYear" value="2025"/> 2025
+            <input type="radio" name="graduationYear" value="2026"/> 2026
+            <input type="radio" name="graduationYear" value="2027"/> 2027
+            <input type="radio" name="graduationYear" value="2028"/> 2028
+
+            <button type="submit">Submit</button>
+        </form>
+
+        <table>
+          <tr>
+            <th>Name</th>
+            <th>Email</th>
+            <th>Graduation Year</th>
+          </tr>
+          <tr>
+            {/* Replace with state variables */}
+          </tr>
+        </table>
+      </div>
     </>
   )
 }
